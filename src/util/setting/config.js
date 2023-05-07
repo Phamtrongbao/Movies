b27b301280a6e51a7e22865728921ac6bd122e9d
trongbao123 <phambao11062001>
2023-05-07 13:57:41 +0700

    first commit

---
+++ src/util/setting/config.js
@@ -0,0 +1,10 @@
+// định nghĩa tham số cố định
+
+// DOmain ở đây là "https://movienew.cybersoft.edu.vn"
+export const DOMAIN = "https://movienew.cybersoft.edu.vn";
+export const TOKEN = "accessToken";
+export const TOKEN_CYBER =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCDEkMOgIE7hurVuZyAwNiIsIkhldEhhblN0cmluZyI6IjE0LzA3LzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY4OTI5MjgwMDAwMCIsIm5iZiI6MTY2MDE1MDgwMCwiZXhwIjoxNjg5NDQwNDAwfQ.nvrySbONO7THMJnLTWgEwiGszUF7VXjBUnn36QUuwsQ";
+export const GROUP = "GP10";
+export const USER_LOGIN = "USER_LOGIN";
+export const USER_REGISTER = "USER_REGISTER";
